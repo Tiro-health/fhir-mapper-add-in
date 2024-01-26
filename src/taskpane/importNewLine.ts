30b1387c628230dc0c49c92e62a3e7b4c18b680f
SVercruyssen <sophie.vercruyssen@cronos.be>
2024-01-26 12:21:46 +0100

    bugfix add new line and do not overwrite next line

--- src/taskpane/importNewLine.ts
+++ src/taskpane/importNewLine.ts
@@ -14,16 +14,18 @@ export default async function importCodingNewLine({ code, display, system }: Cod
     const sheet = context.workbook.worksheets.getActiveWorksheet();
     const activeCell = context.workbook.getActiveCell();
     activeCell.load({ rowIndex: true, columnIndex: true });
+    const activeRow = activeCell.getEntireRow();
     await context.sync();
     const nextRow = activeCell.getRowsBelow();
     nextRow.load({ rowIndex: true, address: true });
     await context.sync();
     // Insert empty row
     // eslint-disable-next-line no-undef
-    // nextRow.getEntireRow().insert(Excel.InsertShiftDirection.down);
-    // Insert duplicate of row
-    nextRow.getEntireRow().copyFrom(activeCell.getEntireRow());
+    nextRow.getEntireRow().insert(Excel.InsertShiftDirection.down).copyFrom(activeRow);
     await context.sync();
+    // Insert duplicate of row
+    // nextRow.getEntireRow().copyFrom(activeCell.getEntireRow());
+    // await context.sync();
     const newCell = sheet.getCell(nextRow.rowIndex, activeCell.columnIndex);
     newCell.valuesAsJson = [[createCodingEntityCard({ code, display, system })]];
     await context.sync();
