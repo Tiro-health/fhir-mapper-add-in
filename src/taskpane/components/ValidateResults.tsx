4a6f01b2c69ac9bda2a648f61367677c5c1ba7b2
SVercruyssen <sophie.vercruyssen@cronos.be>
2024-02-02 11:42:58 +0100

    add coloured circle to validation button

--- src/taskpane/components/ValidateResults.tsx
+++ src/taskpane/components/ValidateResults.tsx
@@ -9,17 +9,17 @@ export default function ValidateResultsButtons() {
   return (
     <section className="flex flex-col">
       <div className="flex gap-x-3">
-        <h4 className="font-semibold text-lg text-gray-500 leading-tight">Validate results</h4>
+        <h4 className="font-semibold text-lg text-gray-500 leading-tight"></h4>
         <div>
           <div>
             <Button key="approve_button" type="button" onClick={() => handleValidationClick(1)}>
-              Green
+              <span>&#128994;</span>
             </Button>
             <Button key="orange_button" type="button" onClick={() => handleValidationClick(0)}>
-              Orange
+              <span>&#128992;</span>
             </Button>
             <Button key="reject_button" type="button" onClick={() => handleValidationClick(-1)}>
-              Red
+              <span>&#128308;</span>
             </Button>
           </div>
         </div>
